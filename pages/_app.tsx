d9e3ccb4cf7612deea6d619bf728c2215e5be3e2
Enes Polat <turksancaktar1071@gmail.com>
2023-05-02 22:55:49 +0300

    iskelet olusuturuldu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,5 +3,10 @@ import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
-  This is the homepage </>
+  <h2>Create Images With Your Mind</h2>
+  <textarea 
+  className="app-input"
+  placeholder="Create any type of image you can think of with as much added description as you would like"/>
+  <button>Generate Image</button>
+  </>
 }
